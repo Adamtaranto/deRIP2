a622f31114ab26947ee5158f1305c5f7c61451c3
Adam Taranto <adam.p.taranto@gmail.com>
2019-02-21 15:15:36 +0000

    reformat for pypi

--- derip2/__init__.py
+++ derip2/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-#python 2.7.5 requires biopython
-#deRIP2.py
-#Version 0.1.0 Adam Taranto, May 2017
+#python 3
+#requires biopython
 #Contact, Adam Taranto, adam.taranto@anu.edu.au
 
 ##################################################################################################################
@@ -12,7 +11,6 @@
 
 import sys
 import os
-import argparse
 from collections import Counter
 from collections import namedtuple
 from copy import deepcopy
@@ -24,6 +22,10 @@ from Bio.Align import AlignInfo #,MultipleSeqAlignment
 from Bio.Alphabet import IUPAC, Gapped #,generic_dna
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+
+__version__ = "0.0.1"
+
+class Error (Exception): pass
 
 def dochecks(usrOutDir):
 	"""Make outDir if does not exist else set to current dir."""
@@ -254,92 +256,3 @@ def writeAlign(tracker,align,outPathAln,ID="deRIPseq",outAlnFormat="fasta"):
 	align.append(deRIPseq)
 	with open(outPathAln, "w") as f:
 		AlignIO.write(align, f, outAlnFormat)
-
-def main(args):
-	# Check for output directories
-	outDir = dochecks(args.outDir)
-	outPathFile = os.path.join(outDir,args.outName)
-	outPathAln = os.path.join(outDir,args.outAlnName)
-	# Read in alignment file, check at least 2 sequences present and names are unique
-	align = loadAlign(args.inAln,args.format)
-	# Initialise object to assemble deRIP'd sequence
-	tracker = initTracker(align)
-	# Initialise object to track RIP observations and GC content by row
-	RIPcounts = initRIPCounter(align)
-	# Preset invariant or highly gapped positions in final sequence
-	tracker = fillConserved(align,tracker,args.maxGaps)
-	# Correct / tally RIP + correct C->T / G->A conversions
-	tracker,RIPcounts = correctRIP(align,tracker,RIPcounts,maxSNPnoise=args.maxSNPnoise,minRIPlike=args.minRIPlike,reaminate=args.reaminate)
-	# Select least RIP'd / most GC-rich sequence in alignment to inherit remaining unset positions from
-	refID = setRefSeq(align, RIPcounts, getMinRIP=True)
-	# Fill remaining unset positions from min RIP / max GC original sequence
-	tracker = fillRemainder(align,refID,tracker)
-	# Write ungapped deRIP to file
-	writeDERIP(tracker,outPathFile,ID=args.label)
-	# Write updated alignment (including gapped deRIP) to file. Optional.
-	writeAlign(tracker,align,outPathAln,ID=args.label,outAlnFormat=args.outAlnFormat)
-
-if __name__== '__main__':
-	###Argument handling.
-	parser = argparse.ArgumentParser(
-									description='Takes a multi-sequence DNA alignment and estimates a progenitor \
-									sequence by correcting for RIP-like mutations.',
-									prog='deRIP2')
-	parser.add_argument("-i", 
-									"--inAln",
-									required=True,
-									type=str,
-									default= None,
-									help="Multiple sequence alignment.")
-	parser.add_argument('--format',
-									default="fasta",
-									choices=["clustal","emboss","fasta","fasta-m10","ig","nexus","phylip","phylip-sequential","phylip-relaxed","stockholm"],
-									help='Format of input alignment.')
-	parser.add_argument('--outAlnFormat',
-									default="fasta",
-									choices=["clustal","emboss","fasta","fasta-m10","ig","nexus","phylip","phylip-sequential","phylip-relaxed","stockholm"],
-									help='Optional: Write alignment including deRIP sequence to file of format X.')
-	parser.add_argument("-g", 
-									"--maxGaps",
-									type=float,
-									default=0.7, 
-									help="Maximum proportion of gapped positions in column to be tolerated before \
-									forcing a gap in final deRIP sequence.")
-	parser.add_argument("-a", 
-									"--reaminate",
-									action="store_true",
-									default=False, 
-									help="Correct deamination events in non-RIP contexts.")
-	parser.add_argument("--maxSNPnoise",
-									type=float,
-									default=0.5,
-									help="Maximum proportion of conflicting SNPs permitted before excluding column \
-									from RIP/deamination assessment. i.e. By default a column with >= 0.5 'C/T' bases \
-									will have 'TpA' positions logged as RIP events."
-									)
-	parser.add_argument("--minRIPlike",
-									type=float,
-									default=0.1,
-									help="Minimum proportion of deamination events in RIP context (5' CpA 3' --> 5' TpA 3') \
-									required for column to deRIP'd in final sequence. Note: If 'reaminate' option is \
-									set all deamination events will be corrected "
-									)
-	parser.add_argument("-o", 
-									"--outName",
-									type=str,
-									default= "deRIP_output.fa", 
-									help="Write deRIP sequence to this file.")
-	parser.add_argument('--outAlnName',
-									default=None,
-									help='Optional: If set write alignment including deRIP sequence to this file.')
-	parser.add_argument('--label',
-									default="deRIPseq",
-									help="Use label as name for deRIP'd sequence in output files.")
-	parser.add_argument("-d", 
-									"--outDir",
-									type=str,
-									default= None, 
-									help="Directory for deRIP'd sequence files to be written to.")
-	args = parser.parse_args()
-
-	main(args)
