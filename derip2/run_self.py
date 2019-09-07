a1cb191911071742fec6fefb8d6dec9e478eac94
Adam Taranto <adam.p.taranto@gmail.com>
2019-09-07 21:23:40 +1000

    Add argparse comments

--- derip2/run_self.py
+++ derip2/run_self.py
@@ -19,15 +19,18 @@ def mainArgs():
 			prog		=	'derip2'
 			)
 	parser.add_argument('--version', action='version',version='%(prog)s {version}'.format(version=__version__))
+	# Inputs
 	parser.add_argument("-i","--inAln",required=True,type=str,default= None,help="Multiple sequence alignment.")
 	parser.add_argument('--format',default="fasta",choices=["clustal","emboss","fasta","fasta-m10","ig","nexus","phylip","phylip-sequential","phylip-relaxed","stockholm"],help='Format of input alignment.')
-	parser.add_argument('--outAlnFormat',default="fasta",choices=["clustal","emboss","fasta","fasta-m10","ig","nexus","phylip","phylip-sequential","phylip-relaxed","stockholm"],help='Optional: Write alignment including deRIP sequence to file of format X.')
+	# Options	
 	parser.add_argument("-g","--maxGaps",type=float,default=0.7, help="Maximum proportion of gapped positions in column to be tolerated before forcing a gap in final deRIP sequence.")
 	parser.add_argument("-a","--reaminate",action="store_true",default=False, help="Correct deamination events in non-RIP contexts.")
 	parser.add_argument("--maxSNPnoise",type=float,default=0.5,help="Maximum proportion of conflicting SNPs permitted before excluding column from RIP/deamination assessment. i.e. By default a column with >= 0.5 'C/T' bases will have 'TpA' positions logged as RIP events.")
 	parser.add_argument("--minRIPlike",type=float,default=0.1,help="Minimum proportion of deamination events in RIP context (5' CpA 3' --> 5' TpA 3') required for column to deRIP'd in final sequence. Note: If 'reaminate' option is set all deamination events will be corrected ")
+	# Outputs
 	parser.add_argument("-o","--outName",type=str,default= "deRIP_output.fa", help="Write deRIP sequence to this file.")
 	parser.add_argument('--outAlnName',default=None,help='Optional: If set write alignment including deRIP sequence to this file.')
+	parser.add_argument('--outAlnFormat',default="fasta",choices=["clustal","emboss","fasta","fasta-m10","ig","nexus","phylip","phylip-sequential","phylip-relaxed","stockholm"],help='Optional: Write alignment including deRIP sequence to file of format X.')
 	parser.add_argument('--label',default="deRIPseq",help="Use label as name for deRIP'd sequence in output files.")
 	parser.add_argument("-d","--outDir",type=str,default= None,help="Directory for deRIP'd sequence files to be written to.")
 	args = parser.parse_args()
