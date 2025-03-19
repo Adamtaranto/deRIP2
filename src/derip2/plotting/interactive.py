c19b41a14cd6280fc6826c50936bb5049f165ffa
Adam Taranto <adam.p.taranto@gmail.com>
2025-03-19 20:43:26 +1100

    rm unimplemented

--- src/derip2/plotting/interactive.py
+++
@@ -1,7 +0,0 @@
-"""
-# TODO: Implement interactive plotting of alignments.
-"""
-# https://dmnfarrell.github.io/bioinformatics/bokeh-sequence-aligner
-# https://plotly.com/python/alignment-chart/
-
-# Plot align object with RIP'd bases and/or other mutations highlighted
